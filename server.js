5869599c4eed00448d057adae3b892602148133f
Edwin Pinchao <miguepinchao1992@gmail.com>
2023-10-10 08:47:23 -0500

    Api Endpoint

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 import express from 'express';
+//import bodyParser from 'body-parser';
 import StatusCodes from 'http-status-codes';
 
 
 const app= express();
-const port = 3000;
-const code=201;
-app.get('/add', (req, res) => {
-    res.status(StatusCodes.CREATED);
+const port = 4000;
+
+const STATUS ={
+    SUCCESS: 'OK',
+    FAILURE: 'NO'
+};
+
+app.use(express.json());
+//app.use(bodyParser.json());
+
+app.get('/hello', (req, res) => {
+    res.status(StatusCodes.OK);
     res.send("GET Request Called")
+});
+
+app.post('/add', (req, res)=>{
+const data =[];
+const {body} = req;
+
+if(!body.name){
+    return res.status(StatusCodes.BAD_REQUEST).send({
+        status: STATUS.FAILURE,
+        message: 'Name is required',
+    })
+}
+data.push(req.body);
+return res.status(StatusCodes.OK).send({
+    status: STATUS.SUCCESS,
+    message: data,
+});
 });
 
 app.listen( port , hostname  =>{
